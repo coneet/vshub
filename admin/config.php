2881781e37d93cbfe4dcdb0fec23c79e3adbdc10
Vineet Sharma <coneecu2@cp-13.webhostbox.net>
2021-01-23 02:25:43 +0000

    initial commit

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://vshub.in/admin/');
+define('HTTP_CATALOG', 'http://vshub.in/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://vshub.in/admin/');
+define('HTTPS_CATALOG', 'http://vshub.in/');
+
+// DIR
+define('DIR_APPLICATION', '/home/coneecu2/public_html/admin/');
+define('DIR_SYSTEM', '/home/coneecu2/public_html/system/');
+define('DIR_IMAGE', '/home/coneecu2/public_html/image/');
+define('DIR_STORAGE', '/home/coneecu2/storage/');
+define('DIR_CATALOG', '/home/coneecu2/public_html/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'coneecu2_vshub');
+define('DB_PASSWORD', 'Vshub@987');
+define('DB_DATABASE', 'coneecu2_vshub');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'vs_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
